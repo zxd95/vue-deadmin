8d61a408688f968a26b923b7c922865baa6b9192
zhangxiaodong <774793337@qq.com>
2019-08-20 12:08:24 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
-
 import 'normalize.css/normalize.css'// A modern alternative to CSS resets
-
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
-import VCharts from 'v-charts'
+import locale from 'element-ui/lib/locale/lang/zh-CN'
 
 import '@/styles/index.scss' // global css
 
